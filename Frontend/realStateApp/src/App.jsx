4937c159c488473f27926c8f811c79f922e02394
Ankit singh <singhankit0460@gmail.com>
2024-11-30 14:44:31 +0530

    navbar

--- Frontend/realStateApp/src/App.jsx
+++ Frontend/realStateApp/src/App.jsx
@@ -1,8 +1,23 @@
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Header from "./component/Header";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
