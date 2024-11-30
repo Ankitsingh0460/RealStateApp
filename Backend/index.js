480c0e80a44c5fe2bf73fe6297c4777cd7566699
Ankit singh <singhankit0460@gmail.com>
2024-11-30 15:41:09 +0530

    database connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,20 @@
-function App() {
-  console.log("no");
-}
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const PORT = process.env.PORT || 3001;
+dotenv.config();
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}`);
+});
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connect to DataBase");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
