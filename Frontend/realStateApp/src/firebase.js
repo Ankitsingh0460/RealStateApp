b15b3180fb6ecdbc8b0538ff45cc01206673f562
Ankit singh <singhankit0460@gmail.com>
2024-12-02 12:42:35 +0530

    firebase

--- Frontend/realStateApp/src/firebase.js
+++ Frontend/realStateApp/src/firebase.js
@@ -7,6 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  //api
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "realestate-30875.firebaseapp.com",
   projectId: "realestate-30875",
