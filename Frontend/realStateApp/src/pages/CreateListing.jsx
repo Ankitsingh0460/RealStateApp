caa53fbe04abff0663c3d26d234e967342404ef7
Ankit singh <singhankit0460@gmail.com>
2024-12-13 02:49:45 +0530

    add create listing  ui funcinality

--- Frontend/realStateApp/src/pages/CreateListing.jsx
+++ Frontend/realStateApp/src/pages/CreateListing.jsx
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 function CreateListing() {
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "rent",
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 50,
+    offer: false,
+    parking: false,
+    furnished: false,
+  });
+  console.log(formData);
+  const { loading, error } = useSelector((state) => state.user);
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "offer" ||
+      e.target.id === "furnished"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+    } catch (error) {
+      next(error);
+    }
+  };
   return (
     <>
       <main className="p-3 max-w-4xl mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7">
           Create a Listing
         </h1>
-        <form className="flex flex-col sm:flex-row gap-4">
+        <form
+          className="flex flex-col sm:flex-row gap-4"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col gap-4 flex-1">
             <input
               placeholder="Name"
@@ -17,13 +74,17 @@ function CreateListing() {
               maxLength="62"
               minLength="10"
               required
+              onChange={handleChange}
+              value={formData.name}
             />
             <textarea
-              placeholder="discription"
+              placeholder="description"
               type="text"
               className="border p-3 rounded-lg "
-              id="discription"
+              id="description"
               required
+              onChange={handleChange}
+              value={formData.description}
             />
             <input
               placeholder="Address"
@@ -31,26 +92,58 @@ function CreateListing() {
               className="border p-3 rounded-lg "
               id="address"
               required
+              onChange={handleChange}
+              value={formData.address}
             />
             <div className="flex gap-6 flex-wrap">
               <div className="flex gap-2">
-                <input type="checkbox" id="sale" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="sale"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.type === "sale"}
+                />
                 <span>Sell</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="rent" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="rent"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.type === "rent"}
+                />
                 <span>Rent</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="parking" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="parking"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.parking}
+                />
                 <span>Parking</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="furnished" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="furnished"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.furnished}
+                />
                 <span>Furnished</span>
               </div>
               <div className="flex gap-2">
-                <input type="checkbox" id="offer" className="w-5" />
+                <input
+                  type="checkbox"
+                  id="offer"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.offer}
+                />
                 <span>Offer</span>
               </div>
             </div>
@@ -63,6 +156,8 @@ function CreateListing() {
                   max="10"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.bedrooms}
                 />
                 <p>Beds</p>
               </div>
@@ -74,6 +169,8 @@ function CreateListing() {
                   max="10"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.bathrooms}
                 />
                 <p>Baths</p>
               </div>
@@ -81,10 +178,12 @@ function CreateListing() {
                 <input
                   type="number"
                   id="regularPrice"
-                  min="1"
-                  max="10"
+                  min="10"
+                  max="100000"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.regularPrice}
                 />
                 <div className="flex flex-col items-center">
                   <p>Regular Price</p>
@@ -94,11 +193,13 @@ function CreateListing() {
               <div className="flex items-center gap-2">
                 <input
                   type="number"
-                  id="discountedPrice"
-                  min="1"
-                  max="10"
+                  id="discountPrice"
+                  min="10"
+                  max="100000"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.discountPrice}
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
