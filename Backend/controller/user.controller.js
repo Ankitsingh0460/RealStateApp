7239050e65db8f1627439c32fc1223d299c7e873
Ankit singh <singhankit0460@gmail.com>
2024-12-13 17:16:35 +0530

    create getUserListing api

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+import Listing from "../models/listing.model.js";
 
 export const update = async (req, res, next) => {
   if (req.user.id !== req.params.id)
@@ -41,6 +42,19 @@ export const deleteUser = async (req, res, next) => {
     const user = await User.findOneAndDelete(req.params.id);
     res.clearCookie("access_token");
     res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  try {
+    if (req.user.id === req.params.id) {
+      const listing = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listing);
+    } else {
+      return next(errorHandler(401, "You can only view your own listing"));
+    }
   } catch (error) {
     next(error);
   }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { update, deleteUser } from "../controller/user.controller.js";
+import {
+  update,
+  deleteUser,
+  getUserListing,
+} from "../controller/user.controller.js";
 import { tokenverify } from "../utils/tokenverify.js";
 
 const router = express.Router();
 
 router.post("/update/:id", tokenverify, update);
 router.delete("/delete/:id", tokenverify, deleteUser);
+router.get("/listing/:id", tokenverify, getUserListing);
 
 export default router;
